f6a4ba98be03229dba5e9963e6bedbb1d1ea9424
callmeocean <33929230+callmeocean@users.noreply.github.com>
2025-05-18 00:02:21 -0700

    Initial website version

---
+++ script.js
@@ -0,0 +1,31 @@
+// Smooth Scrolling for Hero Button and Section Navigation
+document.querySelectorAll('.scroll-btn, .section-nav').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      let target = this.getAttribute('href');
+      const targetSection = document.querySelector(target);
+
+      if (target == '#trailer') {
+        document.getElementById('hero').style.display = 'none';
+
+        let trailer = document.getElementById('trailer');
+
+        trailer.style.display = 'flex';
+
+        let player = new Vimeo.Player(trailer.querySelector('iframe'));
+
+        player.play().then(function() {
+          player.requestFullscreen();
+        });
+      }
+      else {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', () => {
+  let copyrightYear = document.getElementById('copyrightYear');
+
+  copyrightYear.textContent = new Date().getFullYear();
+});
